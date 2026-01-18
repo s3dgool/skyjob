e4c22bd9431e6ca5497af25568296549309bd6ef
s3dgool <s3dgool@users.noreply.github.com>
2026-01-18 16:14:20 +0300

    Set page favicon to logo.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SkyJob",
   description: "Find your dream job",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
